5a98bcb5311529e1addbcc8db686d62ade86d0dc
Muhammed122 <ma2078941@gmail.com>
2023-08-26 17:31:06 +0300

    adding nav graph to app

---
+++ app/src/main/java/com/example/currencyconvertertask/presentation/CurrencyConverterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.currencyconvertertask.presentation
+
+import com.example.currencyconvertertask.databinding.ActivityCurrencyConverterBinding
+import com.example.currencyconvertertask.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CurrencyConverterActivity : BaseActivity<ActivityCurrencyConverterBinding>(
+    ActivityCurrencyConverterBinding::inflate
+) {
+
+
+
+}
